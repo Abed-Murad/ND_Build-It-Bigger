1e115bd242f6f668a4ad5ab38557a7a11cb9f718
Abed Murad <abdallah.Murad@mail.com>
2018-11-24 02:40:44 +0100

    Add ButterKnife Dependenciey

--- androidlibrary/src/main/java/com/am/androidlibrary/JokeActivity.java
+++ androidlibrary/src/main/java/com/am/androidlibrary/JokeActivity.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 
 public class JokeActivity extends AppCompatActivity {
 
+    public final static String EXTRA_JOKE = "joke";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getIntent().hasExtra(EXTRA_JOKE)) {
+            String joke = getIntent().getStringExtra(EXTRA_JOKE);
+
+        }
     }
 }
