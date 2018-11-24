95b923f49de39c027da20fdd68dd7716e04b75be
Abed Murad <abdallah.Murad@mail.com>
2018-11-24 03:05:23 +0100

    Create GetJokeFromServerAsyncTask

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/GetJokeFromServerAsyncTask.java
@@ -0,0 +1,14 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+public class GetJokeFromServerAsyncTask extends AsyncTask {
+    public GetJokeFromServerAsyncTask(Context applicationContext) {
+    }
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
